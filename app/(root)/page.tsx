e25fcdbdea9a6f48935a769a66bc21780b7ee7b4
Antonije Mirkovic <antonije@mirkovic.no>
2024-06-16 15:52:38 +0200

    ignore ts, fixed errors

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,45 +1,54 @@
-import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
-import '../globals.css';
-import { fetchPosts } from '@/lib/actions/thread.actions';
-import { currentUser } from '@clerk/nextjs/server';
-import ThreadCard from '@/components/cards/ThreadCard';
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-export default async function RootLayout({
-  children,
+import ThreadCard from "@/components/cards/ThreadCard";
+
+import { fetchPosts } from "@/lib/actions/thread.actions";
+import { fetchUser } from "@/lib/actions/user.actions";
+
+async function Home({
+  searchParams,
 }: {
-  children: React.ReactNode
+  searchParams: { [key: string]: string | undefined };
 }) {
+  const user = await currentUser();
+  if (!user) return null;
 
-  const result = await fetchPosts(1, 30)
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
 
-  const user = await currentUser()
-
-  console.log(result)
+  const result = await fetchPosts(
+    searchParams.page ? +searchParams.page : 1,
+    30
+  );
 
   return (
     <>
-      <h1 className="head-text text-left">Home</h1>
+      <h1 className='head-text text-left'>Home</h1>
 
-      <section className="mt-9 flex flex-col gap-10">
+      <section className='mt-9 flex flex-col gap-10'>
         {result.posts.length === 0 ? (
-          <p className="no-result">No threads found!</p>
+          <p className='no-result'>No threads found</p>
         ) : (
           <>
-          {result.posts.map((post) => (
-            <ThreadCard 
-            key={post._id}
-            id={post._id}
-            currentUserId={user?.id || ""}
-            parentId={post.parentId}
-            content={post.text}
-            author={post.author}
-            community={post.community}
-            createdAt={post.createdAt}
-            comments={post.children}/>
-          ))}
+            {result.posts.map((post) => (
+              <ThreadCard
+                key={post._id}
+                id={post._id}
+                currentUserId={user.id}
+                parentId={post.parentId}
+                content={post.text}
+                author={post.author}
+                community={post.community}
+                createdAt={post.createdAt}
+                comments={post.children}
+              />
+            ))}
           </>
         )}
       </section>
     </>
-  )
+  );
 }
+
+export default Home;
